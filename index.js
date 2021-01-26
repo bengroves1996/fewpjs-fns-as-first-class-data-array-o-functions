ba560301e9d7d52fe44caeed66e6bccb8eae1d93
bengroves <bengroves15@gmail.com>
2021-01-26 15:42:17 -0500

    commit tree 40043fe010af69608533a1bcefee6cdf4d39a0a7

---
+++ index.js
@@ -0,0 +1,33 @@
+function wakeDog(dogName, dogBreed) {
+    let val = (`Wake ${dogName} the ${dogBreed}`);
+    console.log(val);
+    return val 
+}
+
+function leashDog(dogName, dogBreed){
+    let val = `Leash ${dogName} the ${dogBreed}`
+    console.log(val)
+    return val
+}
+
+function walkToPark(dogName, dogBreed){
+    let val = `Walk to the park with ${dogName} the ${dogBreed}`
+    console.log(val)
+    return val
+}
+function throwFrisbee(dogName, dogBreed){
+    let val = `Throw the frisbee for ${dogName} the ${dogBreed}`
+    console.log(val)
+    return val
+}
+function walkHome(dogName, dogBreed){
+    let val = `Walk home with ${dogName} the ${dogBreed}`
+    console.log(val)
+    return val
+}
+
+function unleashDog(dogName, dogBreed){
+    let val = `Unleash ${dogName} the ${dogBreed}`
+    console.log(val)
+    return val
+}
